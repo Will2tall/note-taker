65d10449951b43e8741e9449d73709802dad470e
Terror666 <ckuthulu@gmail.com>
2021-10-01 16:08:33 -0500

    delete test

--- routes/apiRoutes/note.js
+++ routes/apiRoutes/note.js
@@ -1,20 +1,29 @@
 const router = require('express').Router();
 const { notes } = require('../../db/notes.json')
-const { createNote, deleteNote } = require('../../lib/notes')
+const { createNote, delNote, getNotes } = require('../../lib/notes')
 
 router.get('/notes', (req, res) => {
+   
     let results = notes;
 
     res.json(results);
 });
 
 router.post('/notes', (req, res) => {
+    
     req.body.id = notes.length.toString();
 
     const note = createNote(req.body, notes)
     res.json(note);
     
 });
+
+router.delete('/notes/:id', (req, res) => {
+    
+    const dNote = delNote(req.params.id, notes);
+
+    res.json(dNote);
+})
 
 
 module.exports = router;
