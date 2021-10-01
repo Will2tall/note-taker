9918eb33a680b09774bb471142065129be8c8a45
Terror666 <ckuthulu@gmail.com>
2021-10-01 02:33:19 -0500

    working routes

---
+++ routes/apiRoutes/note.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { notes } = require('../../db/notes.json')
+const { createNote, deleteNote } = require('../../lib/notes')
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+
+    res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNote(req.body, notes)
+    res.json(note);
+    
+});
+
+router.delete('/notes/:id', (req, res) => {
+    id = req.query.id
+})
+
+module.exports = router;
